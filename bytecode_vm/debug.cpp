e9de15c1218c65e591b8d0b62e3e05d7a6907765
wertysas <johanerics@gmail.com>
2023-04-18 12:22:59 +0200

    [BYTECODE VM] OP_CONSTANT_LONG fixed

--- bytecode_vm/debug.cpp
+++ bytecode_vm/debug.cpp
@@ -46,7 +46,7 @@ size_t constant_instruction(const Chunk &chunk, size_t offset) {
 
 size_t constant_instruction_long(const Chunk &chunk, size_t offset) {
     uint8_t* b = reinterpret_cast<uint8_t*>(&chunk.opcodes[offset+1]);
-    size_t const_idx = (b[0] << 16 | b[1] << 8 | b[2]);
+    uint32_t const_idx = (b[0] | b[1] << 8 | b[2] << 16);
     Value val = chunk.constants[const_idx];
     std::cout << "OP_CONSTANT_LONG " << static_cast<unsigned int>(const_idx) << std::endl;
     print_value(val);
