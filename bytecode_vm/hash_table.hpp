7ace58aa98b514d09f28dfd36e91416a28f007df
wertysas <johanerics@gmail.com>
2023-07-05 10:13:04 +0200

    [Bugfix] Hash Table bug fixed 100/100 on tests (tombstone implementation)

--- bytecode_vm/hash_table.hpp
+++ bytecode_vm/hash_table.hpp
@@ -179,7 +179,7 @@ TableEntry<Key, T>& HashTable<Key, T, Hash, KeyEqual, A>::find(Key const& key) c
                    EntryType::EMPTY) {    // return previous tombstone or entry
             return tombstone != nullptr ? *tombstone : *entry;
         }
-        if (tombstone == nullptr) {    // else we have a tombstone
+        if (tombstone == nullptr && type != EntryType::USED) {
             tombstone = entry;
         }
         idx = (idx + 1) % capacity_; // possible optimization (mod in hot loop)
